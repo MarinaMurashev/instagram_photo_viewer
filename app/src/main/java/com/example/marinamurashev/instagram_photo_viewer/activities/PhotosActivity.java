c3ac09032c7793920c4659bd8fd84379874803c0
Marina Murashev <marina@hoteltonight.com>
2015-02-07 21:27:40 -0800

    gets rid of menu

--- app/src/main/java/com/example/marinamurashev/instagram_photo_viewer/activities/PhotosActivity.java
+++ app/src/main/java/com/example/marinamurashev/instagram_photo_viewer/activities/PhotosActivity.java
@@ -2,8 +2,6 @@ package com.example.marinamurashev.instagram_photo_viewer.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.example.marinamurashev.instagram_photo_viewer.R;
@@ -31,27 +29,5 @@ public class PhotosActivity extends ActionBarActivity {
 
         new InstagramPopularMediaService(instagramPhotosAdapter, instagramPhotos).fetchPopularPhotos();
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
